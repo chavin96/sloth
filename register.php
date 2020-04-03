0132dcab218ca22571d0a621b312f7b25faec3a1
Extrogene <chavin@extrogene.com>
2020-04-03 23:10:43 +0530

    Registration code clean

--- register.php
+++ register.php
@@ -1,3 +1,39 @@
+<?php
+
+function sanitizeFormUsername($inputText)
+{
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ", "", $inputText);
+    return $inputText;
+}
+function sanitizeFormString($inputText)
+{
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ", "", $inputText);
+    $inputText = ucfirst(strtolower($inputText));
+    return $inputText;
+}
+function sanitizeFormPassword($inputText)
+{
+    $inputText = strip_tags($inputText);
+    return $inputText;
+}
+
+if (isset($_POST['loginButton'])) {
+    echo "login button is pressed";
+}
+
+if (isset($_POST['registerButton'])) {
+    $username = sanitizeFormUsername($_POST['username']);
+    $firstName = sanitizeFormString($_POST['firstName']);
+    $lastName = sanitizeFormString($_POST['lastName']);
+    $email = sanitizeFormString($_POST['email']);
+    $email2 = sanitizeFormString($_POST['email2']);
+    $password = sanitizeFormPassword($_POST['password']);
+    $password2 = sanitizeFormPassword($_POST['password2']);
+}
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -8,12 +44,10 @@
 <body>
     <div id="inputContainer">
         <form id="loginForm" action="register.php" method="POST">
-            <h1>HI</h1>
-
             <h2>Login to your account</h2>
             <p>
                 <label for="loginUsername">Username</label>
-                <input id="loginUsername" name="loginUsername" type="text" placeholder="Chavin Wick" required>
+                <input id="loginUsername" name="loginUsername" type="text" placeholder="ChavinW" required>
             </p>
             <p>
                 <label for="loginPassword">Password</label>
@@ -21,6 +55,40 @@
             </p>
             <button type="submit" name="loginButton">LOG IN</button>
         </form>
+
+        <form id="registerForm" action="register.php" method="POST">
+            <h2>Create your free account</h2>
+            <p>
+                <label for="username">Username</label>
+                <input id="username" name="username" type="text" placeholder="ChavinW" required>
+            </p>
+            <p>
+                <label for="firstName">First Name</label>
+                <input id="firstName" name="firstName" type="text" placeholder="Chavin" required>
+            </p>
+            <p>
+                <label for="lastName">Last Name</label>
+                <input id="lastName" name="lastName" type="text" placeholder="Wick" required>
+            </p>
+            <p>
+                <label for="email">Email</label>
+                <input id="email" name="email" type="email" placeholder="jon@gmail.com" required>
+            </p>
+            <p>
+                <label for="email2">Confirm Email</label>
+                <input id="email2" name="email2" type="email" placeholder="jon@gmail.com" required>
+            </p>
+            <p>
+                <label for="password">Password</label>
+                <input id="password" name="password" type="password" required>
+            </p>
+            <p>
+                <label for="password2">Confirm Password</label>
+                <input id="password2" name="password2" type="password" required>
+            </p>
+            <button type="submit" name="registerButton">SIGN UP</button>
+        </form>
+
     </div>
 </body>
 
