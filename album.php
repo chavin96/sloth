2a1a923348345a34a403b567887154e9111af00a
Extrogene <chavin@extrogene.com>
2021-02-06 09:26:13 +0530

    Songs update

--- album.php
+++ album.php
@@ -7,9 +7,43 @@ if (isset($_GET['id'])) {
 $album = new Album($con, $albumId);
 
 $artist = $album->getArtist();
-
-echo $album->getTitle();
-echo $artist->getName();
-
 ?>
+<div class="entityInfo">
+    <div class="leftSection">
+        <img src="<?php echo $album->getArtworkPath(); ?>">
+    </div>
+    <div class="rightSection">
+        <h2><?php echo $album->getTitle(); ?></h2>
+        <p><?php echo $artist->getName(); ?></p>
+        <p><?php echo $album->getNumberOfSongs(); ?> songs</p>
+    </div>
+</div>
+<div class="trackListContainer">
+    <ul class="trackList">
+        <?php $songIdArray = $album->getSongIds();
+        $i = 1;
+        foreach ($songIdArray as $songId) {
+            $albumSong = new Song($con, $songId);
+            $albumArtist = $albumSong->getArtist();
+            echo "<li class='trackListRow'>
+            <div class='trackCount'>
+            <img class='play' src='assests/images/icons/play-white.png'>
+            <span class='trackNumber'>$i<span>
+            </div>
+            <div class='trackInfo'>
+                <span class='trackName'>" . $albumSong->getTitle() . "</span>
+                <span class='artistName'>" . $albumArtist->getName() . "</span>
+            </div>
+            <div class='trackOptions'>
+                <img class='optionsButton' src='assests/images/icons/more.png'>    
+            </div>
+            <div class='trackDuration'>
+            <span class='duration'>" . $albumSong->getDuration() . "</span>    
+            </div>
+            </li>";
+            $i++;
+        }
+        ?>
+    </ul>
+</div>
 <?php include("includes/footer.php"); ?>
